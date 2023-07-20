0051b48dd2d0154483596afee3dd587cffdcf3f9
vivek <vrana3325@gmail.com>
2023-07-20 18:47:22 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Comps/header";
+import Pool from "./Comps/pool";
+import Home from "./Comps/home";
+import Groom from "./Comps/groom";
+import Boarding from "./Comps/boarding";
+import Details from "./Comps/details";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+        <Route exact path="" element={<Home/>} />
+          <Route exact path="/Comps/Home" element={<Home/>} />
+          <Route exact path="/Comps/pool" element={<Pool />} />
+          <Route exact path="/Comps/groom" element={<Groom />} />
+          <Route exact path="/Comps/boarding" element={<Boarding />} />
+          <Route exact path="/Comps/details" element={<Details />} />
+
+        </Routes>
+      </Router>
+    </>
   );
 }
 
